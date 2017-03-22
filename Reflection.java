1f4186f10ae3e54c1dbb21d1c17a702daea8255c
Ivy Chang <ivycwq@icloud.com>
2017-03-22 10:46:44 -0700

    using getSuperclass and getClasses methods

--- Reflection.java
+++ Reflection.java
@@ -1,12 +1,26 @@
 public class Reflection {
   public static void main(String[] args) {
     System.out.println("Getting the canonical name of the class...");
-    Class type = null;
+    Class<?> type = null;
     try {
       type = Class.forName("ConcreteClass");
     } catch(ClassNotFoundException e) {
       e.printStackTrace();
     }
     System.out.println(type.getCanonicalName());
+    System.out.println();
+
+    System.out.println("Getting the super class of the class...");
+    System.out.println(type.getSuperclass());
+    System.out.println();
+
+    System.out.println("Getting the public interface members inherited from" + 
+      "super classes and the public class and interface members declared by the class...");
+    Class<?>[] classes = type.getClasses();
+    for(Class<?> c : classes)
+      System.out.println(c);
+    System.out.println();
+
+        
   }
 }

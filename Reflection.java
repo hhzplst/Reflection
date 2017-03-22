5fa8753b01fc8d23731ca294879f58f2fced16b0
Ivy Chang <ivycwq@icloud.com>
2017-03-22 12:10:28 -0700

    adding output

--- Reflection.java
+++ Reflection.java
@@ -7,8 +7,8 @@ public class Reflection {
     Class<?> type = null;
     try {
       type = Class.forName("ConcreteClass");
-    } catch(ClassNotFoundException e) {
-      e.printStackTrace();
+    } catch(Exception e) {
+      System.err.println(e);
     }
     System.out.println(type.getCanonicalName());
     System.out.println();
@@ -69,9 +69,97 @@ public class Reflection {
     ConcreteClass c = null;
     try{
       c = (ConcreteClass)constructors[0].newInstance(10);      
-    } catch (Exception e) {
+    } catch(Exception e) {
       System.err.println(e);
     }
     System.out.println("New instance created with integer " + c.publicInt); 
+    System.out.println();
+
+    System.out.println("Getting the public fields and use the Field claass to get and set the public fields...");
+    try {
+      int original = (int)fields[0].get(c);
+      fields[0].set(c, 15);  
+      System.out.println("Instance variable has been set to " + c.publicInt + " from " + original);
+    } catch(Exception e) {
+      System.err.println(e);
+    } 
+    System.out.println();
+
+    System.out.println("Getting the public methods and use the Method claass to involke public methods...");
+    try {
+      Method m5 = type.getMethod("method5", int.class);
+      System.out.println("Involking method5 with return value " + (int)m5.invoke(c, 3));
+    } catch(Exception e) {
+      System.err.println(e);
+    }
   }
 }
+
+/*******************************************************OUTPUT*******************************************************
+
+➜  AssignmentD git:(master) ✗ java Reflection      
+Getting the canonical name of the class...
+ConcreteClass
+
+Getting the super class of the class...
+class BaseClass
+
+Getting the public interface members inherited fromsuper classes and the public class and interface members declared by the class...
+interface ConcreteClass$ConcreteClassPublicInterface
+class ConcreteClass$ConcreteClassPublicEnum
+class ConcreteClass$ConcreteClassPublicClass
+class BaseClass$BaseClassMemberEnum
+class BaseClass$BaseClassInnerClass
+
+Getting all the classes and interfaces declaredas memebers of the class(not inherited)...
+interface ConcreteClass$ConcreteClassPublicInterface
+class ConcreteClass$ConcreteClassPublicEnum
+class ConcreteClass$ConcreteClassDefultEnum
+class ConcreteClass$ConcreteClassDefualtClass
+class ConcreteClass$ConcreteClassProtectedClass
+class ConcreteClass$ConcreteClassPrivateClass
+class ConcreteClass$ConcreteClassPublicClass
+
+Getting the class modifier...
+1
+
+Getting the implemented interface...
+BaseInterface
+
+Getting the public methods...
+public int ConcreteClass.method5(int)
+public void ConcreteClass.method1()
+public int ConcreteClass.method2(java.lang.String)
+public int ConcreteClass.method4()
+public static void BaseClass.method5()
+public final void java.lang.Object.wait(long,int) throws java.lang.InterruptedException
+public final native void java.lang.Object.wait(long) throws java.lang.InterruptedException
+public final void java.lang.Object.wait() throws java.lang.InterruptedException
+public boolean java.lang.Object.equals(java.lang.Object)
+public java.lang.String java.lang.Object.toString()
+public native int java.lang.Object.hashCode()
+public final native java.lang.Class java.lang.Object.getClass()
+public final native void java.lang.Object.notify()
+public final native void java.lang.Object.notifyAll()
+
+Getting the public constructors...
+public ConcreteClass(int)
+
+Getting the public fields...
+public int ConcreteClass.publicInt
+public static final int BaseInterface.interFaceInt
+
+Getting the class annotations...
+@java.lang.Deprecated()
+
+Getting the constructors and use the Constructor to create a new instance...
+New instance created with integer 10
+
+Getting the public fields and use the Field claass to get and set the public fields...
+Instance variable has been set to 15 from 10
+
+Getting the public methods and use the Method claass to involke public methods...
+method5 overriden.
+Involking method5 with return value 0
+
+*******************************************************END OUTPUT*******************************************************/
